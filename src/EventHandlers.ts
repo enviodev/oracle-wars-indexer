1057a33e5bc336c1a26688854d367ba536e2c6b4
moose-code <jonjonclark@gmail.com>
2025-08-14 14:15:56 +0100

    deploy to prod

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -3,6 +3,8 @@ import {
   ChainlinkProxy_AnswerUpdated,
   RedstoneProxy,
   RedstoneProxy_ValueUpdate,
+  Chronicle_ETH_USD_3,
+  Chronicle_ETH_USD_3_Poked,
 } from "generated";
 
 ChainlinkProxy.AnswerUpdated.handler(async ({ event, context }) => {
@@ -21,6 +23,43 @@ ChainlinkProxy.AnswerUpdated.handler(async ({ event, context }) => {
   };
 
   context.ChainlinkProxy_AnswerUpdated.set(entity);
+});
+
+Chronicle_ETH_USD_3.Poked.handler(async ({ event, context }) => {
+  const nativeTokenUsed =
+    event.transaction.gasUsed * event.transaction.effectiveGasPrice;
+
+  const scaledVal = event.params.val / 10000000000n; // 18 -> 8 decimals
+
+  const entity: Chronicle_ETH_USD_3_Poked = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    caller: event.params.caller,
+    val: scaledVal,
+    age: Number(event.params.age),
+    updatedAt: event.params.age,
+    nativeTokenUsed: nativeTokenUsed,
+  };
+
+  context.Chronicle_ETH_USD_3_Poked.set(entity);
+});
+
+Chronicle_ETH_USD_3.OpPoked.handler(async ({ event, context }) => {
+  const nativeTokenUsed =
+    event.transaction.gasUsed * event.transaction.effectiveGasPrice;
+
+  const [val, age] = event.params.pokeData;
+  const scaledVal = val / 10000000000n; // 18 -> 8 decimals
+
+  const entity: Chronicle_ETH_USD_3_Poked = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    caller: event.params.caller,
+    val: scaledVal,
+    age: Number(age),
+    updatedAt: age,
+    nativeTokenUsed,
+  };
+
+  context.Chronicle_ETH_USD_3_Poked.set(entity);
 });
 
 RedstoneProxy.ValueUpdate.handler(async ({ event, context }) => {
